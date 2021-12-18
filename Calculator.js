dc096a2401c7d7f9db61f3a892ce3f5a3e6409e3
Arshroop Singh Saini <77531443+Arshroop-Singh@users.noreply.github.com>
2021-12-18 11:00:57 +0530

    Debugging the app.js

--- Calculator.js
+++ Calculator.js
@@ -4,27 +4,18 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+app.get('/', (req,res) => {
+res.sendFile(__dirname + '/index.html')
 })
 
-app.post('/', (req,res) => {
-  var num1= Number(req.body.first)
-  var num2= Number(req.body.second)
-  var sum= num1+num2
-  console.log("the sum of the two numers is: "+ sum)
-})
-
-app.get('/bmiCalculator', (req,res) => {
-res.sendFile(__dirname + '/bmi.html')
-})
-
-app.post('/bmiCalculator',(req,res)=>{
-  var weight= Number(req.body.n1);
-  var height= Number(req.body.n2);
-  var bmi= weight/Math.pow(height,2);
+app.post('/',(req,res)=>{
+  var weight= Number(req.body.first);
+  var height= Number(req.body.second);
+  var bmi= weight/(Math.pow(height,2));
   
- console.log("Your BMI is: "+ bmi)
+ res.send("<h1>Your BMI is: </h1>"+ bmi)
+ 
+ res.redirect('/')
 })
 
 app.listen(port, () => {
